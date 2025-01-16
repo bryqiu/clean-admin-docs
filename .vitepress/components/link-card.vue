6f7213b53599f72ce25612b93418dde9c057721c
fifteen <qfifteen@163.com>
2025-01-16 17:38:37 +0800

    chore: docs init

---
+++ .vitepress/components/link-card.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const name = ref('222');
+</script>
+
+<template>
+  <div class="text-lg font-bold">{{ name }}</div>
+</template>
+
+<style module lang="scss"></style>
---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,18 @@
+import { h } from 'vue';
+import type { Theme } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+import './styles/index.css';
+import 'virtual:group-icons.css';
+// import '../../../src/styles/common/tailwind.css';
+
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    });
+  },
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  },
+} satisfies Theme;
